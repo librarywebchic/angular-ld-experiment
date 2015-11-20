b90b215a137281e97b5552cfe5423ed9561d2212
librarywebchic <librarywebchic@192.168.1.131>
2015-11-20 13:21:49 -0600

    Added search form. Doesn't return data properly yet.

--- js/controllers.js
+++ js/controllers.js
@@ -1,29 +1,44 @@
-app.controller('ldcontroller', function($scope, $http) {
-      kb = $rdf.graph();
-      bib = 'http://www.worldcat.org/oclc/7977212';
-      
-      $http({
-    	  method: 'GET',
-    	  url: 'http://experiment.worldcat.org/oclc/7977212.rdf',
-    	  //url: bib,
-    	  headers: {
-    		   'Accept': 'application/rdf+xml'
-    		 },
-    	}).then(function successCallback(response) {
-    		$rdf.parse(response.data, kb, bib, 'application/rdf+xml');   
-            $scope.name = kb.the($rdf.sym(bib), $rdf.sym('http://schema.org/name')).value;
-            $scope.author = kb.the(kb.the($rdf.sym(bib), $rdf.sym('http://schema.org/creator')), $rdf.sym('http://schema.org/name')).value;
-            $scope.datePublished = kb.the($rdf.sym(bib), $rdf.sym('http://schema.org/datePublished')).value;
-            subjectNodes = kb.each($rdf.sym(bib), $rdf.sym('http://schema.org/about'));
-            subjects = [];
-            for (i = 0; i < subjectNodes.length; i++) {
-                if (kb.the(subjectNodes[i], $rdf.sym('http://schema.org/name'))) {
-                 subjects.push(kb.the(subjectNodes[i], $rdf.sym('http://schema.org/name')));
-                }
-            }
-            $scope.subjects = subjects;
-    	  }, function errorCallback(response) {
-    		alert('Failed');
-    		console.log(response);
-    	  });
-    });
+app.controller('ldcontroller', ['$scope', '$http', function($scope, $http) {
+	  $scope.entity = []
+	  $scope.entity.id = '7977212';
+	  $scope.entity.type = 'bib';
+	  
+	  $scope.submit = function() {
+	      kb = $rdf.graph();
+	      if ($scope.entity.type == 'bib'){
+	      	uri = 'http://www.worldcat.org/oclc/' + $scope.entity.id;
+	      	request_url = 'http://experiment.worldcat.org/oclc/' + $scope.entity.id + '.rdf'
+	      	$scope.graphType = 'Bibliographic Record Graph';
+	      } else {
+	        uri = 'http://worldcat.org/entity/work/id/' + $scope.entity.id;
+	        request_url = 'http://experiment.worldcat.org/entity/work/data/' + $scope.entity.id + '.rdf'
+	        $scope.graphType = 'Work Graph';
+	      }
+	
+	      $http({
+	    	  method: 'GET',
+	    	  url: request_url,
+	    	  headers: {
+	    		   'Accept': 'application/rdf+xml'
+	    		 },
+	    	}).then(function successCallback(response) {
+	    		$rdf.parse(response.data, kb, uri, 'application/rdf+xml');   
+	            $scope.name = kb.the($rdf.sym(uri), $rdf.sym('http://schema.org/name')).value;
+	            $scope.author = kb.the(kb.the($rdf.sym(uri), $rdf.sym('http://schema.org/creator')), $rdf.sym('http://schema.org/name')).value;
+	            $scope.datePublished = kb.the($rdf.sym(uri), $rdf.sym('http://schema.org/datePublished')).value;
+	            subjectNodes = kb.each($rdf.sym(uri), $rdf.sym('http://schema.org/about'));
+	            subjects = [];
+	            for (i = 0; i < subjectNodes.length; i++) {
+	                if (kb.the(subjectNodes[i], $rdf.sym('http://schema.org/name'))) {
+	                 subjects.push(kb.the(subjectNodes[i], $rdf.sym('http://schema.org/name')));
+	                }
+	            }
+	            $scope.subjects = subjects;
+	          console.log('In the $http function ' + $scope.subjects);  
+	    	  }, function errorCallback(response) {
+	    		alert('Failed');
+	    		console.log(response);
+	    	  });
+	      console.log($scope.subjects);
+	  };
+    }]);
