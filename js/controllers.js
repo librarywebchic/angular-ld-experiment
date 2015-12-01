87be2f2c4a0769556a876af610931b2b1ca71b5a
Karen Coombs <librarywebchic@gmail.com>
2015-12-01 14:42:55 -0600

    Improved the namespace handling

--- js/controllers.js
+++ js/controllers.js
@@ -4,6 +4,9 @@ app.controller('ldcontroller', ['$scope', '$http', function($scope, $http) {
 	  $scope.entity.type = 'bib';
 	  
 	  $scope.submit = function() {
+		  var SCHEMA = $rdf.Namespace("http://schema.org/")
+		  var WDRS = $rdf.Namespace("http://www.w3.org/2007/05/powder-s#")
+		  
 	      kb = $rdf.graph();
 	      if ($scope.entity.type == 'bib'){
 	      	uri = 'http://www.worldcat.org/oclc/' + $scope.entity.id;
@@ -23,36 +26,36 @@ app.controller('ldcontroller', ['$scope', '$http', function($scope, $http) {
 	    		 },
 	    	}).then(function successCallback(response) {
 	    		$rdf.parse(response.data, kb, uri, 'application/rdf+xml');   
-	            $scope.name = kb.the($rdf.sym(uri), $rdf.sym('http://schema.org/name')).value;
-	            $scope.author = kb.the(kb.the($rdf.sym(uri), $rdf.sym('http://schema.org/creator')), $rdf.sym('http://schema.org/name')).value;
-	            $scope.datePublished = kb.the($rdf.sym(uri), $rdf.sym('http://schema.org/datePublished')).value;
-	            subjectNodes = kb.each($rdf.sym(uri), $rdf.sym('http://schema.org/about'));
+	            $scope.name = kb.the($rdf.sym(uri), SCHEMA('name')).value;
+	            $scope.author = kb.the(kb.the($rdf.sym(uri), SCHEMA('creator')), $rdf.sym('http://schema.org/name')).value;
+	            $scope.datePublished = kb.the($rdf.sym(uri), SCHEMA('datePublished')).value;
+	            subjectNodes = kb.each($rdf.sym(uri), SCHEMA('about'));
 	            subjects = [];
 	            for (i = 0; i < subjectNodes.length; i++) {
-	                if (kb.the(subjectNodes[i], $rdf.sym('http://schema.org/name'))) {
-	                 subjects.push(kb.the(subjectNodes[i], $rdf.sym('http://schema.org/name')));
+	                if (kb.the(subjectNodes[i], SCHEMA('name'))) {
+	                 subjects.push(kb.the(subjectNodes[i], SCHEMA('name')));
 	                }
 	            }
 	            $scope.subjects = subjects;
 	            
-	            $scope.bookEdition = kb.the($rdf.sym(uri), $rdf.sym('http://schema.org/bookEdition')).value;
-	            $scope.copyrightYear = kb.the($rdf.sym(uri), $rdf.sym('http://schema.org/copyrightYear')).value;
-	            $scope.work = kb.the($rdf.sym(uri), $rdf.sym('http://schema.org/exampleOfWork')).uri;
-	            $scope.genre = kb.the($rdf.sym(uri), $rdf.sym('http://schema.org/genre')).value;
-	            $scope.inLanguage = kb.the($rdf.sym(uri), $rdf.sym('http://schema.org/inLanguage')).value;
-	            $scope.publisher = kb.the(kb.the($rdf.sym(uri), $rdf.sym('http://schema.org/publisher')), $rdf.sym('http://schema.org/name')).value;
-	            $scope.dataSet = kb.the(kb.the($rdf.sym(uri), $rdf.sym('http://www.w3.org/2007/05/powder-s#describedby')), $rdf.sym('http://rdfs.org/ns/void#inDataset')).uri;
+	            $scope.bookEdition = kb.the($rdf.sym(uri), SCHEMA('bookEdition')).value;
+	            $scope.copyrightYear = kb.the($rdf.sym(uri), SCHEMA('copyrightYear')).value;
+	            $scope.work = kb.the($rdf.sym(uri), SCHEMA('exampleOfWork')).uri;
+	            $scope.genre = kb.the($rdf.sym(uri), SCHEMA('genre')).value;
+	            $scope.inLanguage = kb.the($rdf.sym(uri), SCHEMA('inLanguage')).value;
+	            $scope.publisher = kb.the(kb.the($rdf.sym(uri), SCHEMA('publisher')), SCHEMA('name')).value;
+	            $scope.dataSet = kb.the(kb.the($rdf.sym(uri), WDRS('describedby')), $rdf.sym('http://rdfs.org/ns/void#inDataset')).uri;
 	            
-	            contributorNodes = kb.each($rdf.sym(uri), $rdf.sym('http://schema.org/contributor'));
+	            contributorNodes = kb.each($rdf.sym(uri), SCHEMA('contributor'));
 	            contributors = [];
 	            for (i = 0; i < contributorNodes.length; i++) {
-	                if (kb.the(contributorNodes[i], $rdf.sym('http://schema.org/name'))) {
-	                	contributors.push(kb.the(contributorNodes[i], $rdf.sym('http://schema.org/name')));
+	                if (kb.the(contributorNodes[i], SCHEMA('name'))) {
+	                	contributors.push(kb.the(contributorNodes[i], SCHEMA('name')));
 	                }
 	            }
 	            $scope.contributors = contributors;
 	            
-	            descriptionNodes = kb.each($rdf.sym(uri), $rdf.sym('http://schema.org/description'));
+	            descriptionNodes = kb.each($rdf.sym(uri), SCHEMA('description'));
 	            descriptions = [];
 	            for (i = 0; i < descriptionNodes.length; i++) {
 	                if (descriptionNodes[i].value) {
@@ -61,10 +64,10 @@ app.controller('ldcontroller', ['$scope', '$http', function($scope, $http) {
 	            }
 	            $scope.descriptions = descriptions;
 	            
-	            productModelNodes = kb.each($rdf.sym(uri), $rdf.sym('http://schema.org/workExample'));
+	            productModelNodes = kb.each($rdf.sym(uri), SCHEMA('workExample'));
 	            isbns = [];
 	            for (i = 0; i < productModelNodes.length; i++) {
-	            	isbnNodes = kb.each(productModelNodes[i], $rdf.sym('http://schema.org/isbn'));
+	            	isbnNodes = kb.each(productModelNodes[i], SCHEMA('isbn'));
 	                if (isbnNodes) {
 	                	for (n = 0; n < isbnNodes.length; n++) {
 	                		isbns.push(isbnNodes[n].value);
